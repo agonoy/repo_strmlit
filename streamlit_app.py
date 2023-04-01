9fad042e4ef70f2b3fae6666b8b046783bdc3aa2
agonoy <kenshen929@icloud.com>
2023-04-01 11:50:36 -1000

    added .env

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 
 import streamlit as st
 
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+
+
 st.write("Hello, world!")
+st.write(openai_api_key)
