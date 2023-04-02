1c74b0ef43f1174ceff5737862da9f396f1420af
agonoy <kenshen929@icloud.com>
2023-04-02 02:03:34 -1000

    key key

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,25 @@
 import streamlit as st
 
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 
-load_dotenv()
-openai_api_key = os.getenv('OPENAI_API_KEY')
+#load_dotenv()
+#openai_api_key = os.getenv('OPENAI_API_KEY')
 
 
 
 st.write("Hello, world!")
-st.write(openai_api_key)
+#st.write(openai_api_key)
+
+import streamlit as st
+import os
+
+if 'env' not in st.secrets:
+    st.error('No secrets were found.')
+else:
+    secrets = st.secrets['env']
+    api_key = secrets['API_KEY']
+
+
+st.write(api_key)
