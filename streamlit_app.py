9f3a2f855fda38167a0370e29bab453f0b754fd3
agonoy <kenshen929@icloud.com>
2023-04-03 10:40:51 -1000

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,18 @@ from dotenv import load_dotenv
 from llama_index import SimpleDirectoryReader
 
 
+# Read the value of the environment variable
+api_key = os.environ["MY_API2"]
 
-api_key = os.environ["API_MY"]
+
+
 #api_key2 = os.environ["MY_API_KEY"]
+
 
 #load_dotenv()
 #openai_api_key = os.getenv('OPENAI_API_KEY')
 
-openai_api_key = os.getenv('NAME_API')
+#openai_api_key = os.getenv('NAME_API')
 
 st.write("Hello, world!")
 #st.write(openai_api_key)
@@ -27,5 +31,5 @@ else:
     secrets = st.secrets['env']
     api_key = secrets['NAME_API']
 
-st.write(openai_api_key)
-st.write(api_key2)
+#st.write(openai_api_key)
+st.write(api_key)
